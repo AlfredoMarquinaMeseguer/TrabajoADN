cf9be16f41d06407f20d98bbfe5b5530854ebcb1
DaniST0 <danielmartinezprior@gmail.com>
2022-12-11 14:11:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import re as re
-import urllib.request
 
-#url = 'https://aulavirtual.um.es/access/content/group/1896_G_2022_N_N/PRACTICAS/PRACTICA%202/All_C_genes_DNA.txt'
+# url = 'https://aulavirtual.um.es/access/content/group/1896_G_2022_N_N/PRACTICAS/PRACTICA%202/All_C_genes_DNA.txt'
 
-# Implementar la
-dicc = {"clave":[[],[]]}
+dicc = {"clave": [[], []]}
+diccGenes = {}
 
 # dicc["clave"][0].append(2)
 # dicc["clave"][1].append("Hola")
@@ -16,19 +11,21 @@ dicc = {"clave":[[],[]]}
 fichero_encimas = "link_bionet.txt"
 fichero_cadenas_C = "All_C_genes_DNA.txt"
 
+
 def rellenaDiccGenes():
     print("Cargando bionet...")
-    # No Abrir fichero por lineas
-    abrir_fichero("All_C_genes_DNA.txt")
-    # Cogemos todos los genes
-
+    # Abre el fichero completo
+    with open(fichero_cadenas_C) as fichero:
+        # Cogemos todos los genes
+        genes = re.split(r"(?<=\n)>.*\n((\w|\s)+)(?=\n)", fichero.read())
     # Bucle que recorra todos los genes
-    # for i in :
+    for gen in genes:
+        separado = re.search(r">(C\.(\w*)) *((\d+) nt)\n([ATGC\s]+)", gen)
         # Reemplazar los espacios del grupo seis
-        #   guardar = re.sub("\s", "", grupo6)
-
+        nucleotidos = re.sub(r"\s", "", separado.group(6))
         # Guardar en diccionario clave grupo 1 y contenido grupo seis reemplazado
-    return 0
+        diccGenes[separado.group(1)] = nucleotidos
+
 
 def rellenaDiccEnzimas():
     print("Cargando All_C_genes_DNA.txt...")
@@ -37,65 +34,67 @@ def rellenaDiccEnzimas():
     # Separar por nombre de enzima
 
     # Bucle guardar en diccionario
-        # Saltar diez primera lineas
-        # Expresion cada linea "([A-Z]\w*) (\(.+\))? +([A-Z^]+)"
-            #  1 -> Clave
-            #  3 -> contenido
+    # Saltar diez primera lineas
+    # Expresion cada linea "([A-Z]\w*) (\(.+\))? +([A-Z^]+)"
+    #  1 -> Clave
+    #  3 -> contenido
 
-        # Sustitución grupo 3 -> diana
-            # Guardar posición ^ y suprimir
-            # llamar reeemplazar por enzimas
-            # Añadir parentesis
+    # Sustitución grupo 3 -> diana
+    # Guardar posición ^ y suprimir
+    # llamar reeemplazar por enzimas
+    # Añadir parentesis
 
-        # Estructura dicc {clave: [[0], "(GGATCC)"]}
-        # Comprobación si existe ya entrada en dicc
-        # if dicc[clave] :
-            # Si Existe añadir grupo3 al string con separador '|'
-                # str.append("|"+diana)
-                # listadeCortes.append(posCorte)
-            # Sino existe crear estrucutra nueva entrada
-                # dicc[clave] = [[posCorte],diana]
+    # Estructura dicc {clave: [[0], "(GGATCC)"]}
+    # Comprobación si existe ya entrada en dicc
+    # if dicc[clave] :
+    # Si Existe añadir grupo3 al string con separador '|'
+    # str.append("|"+diana)
+    # listadeCortes.append(posCorte)
+    # Sino existe crear estrucutra nueva entrada
+    # dicc[clave] = [[posCorte],diana]
     # Fin Bucle
 
 
-def consultaGenes() :
+def consultaGenes():
     gen = input("Gen >> ")
 
-    while(gen != "") :
+    while gen != "":
         # Comprobar que existe en diccionario
-            # Sino está
-                # print("Nombre de gen incorreto")
-            # Si está
-                # print("-------------- "+dicc[clave].length()+" nucleótidos\n"+ \
-                # dicc[gen]+ \
-                # "\n--------------")
-                # consultaEnzimas(dicc[gen])
+        # Si no está
+        if not diccGenes[gen]:
+            print("Nombre de gen incorreto")
+        # Si está
+        else:
+            # Se imprime el numero de nucleotidos y la cadena del gen en el formato especificado
+            print("-------------- " + diccGenes[gen].length() + " nucleótidos\n" +
+                  diccGenes[gen] +
+                  "\n--------------")
+        # Llama a la funcion consulta enzimas
+        consultaEnzimas(diccGenes[gen])
 
-        # volver a pedir otro gen
         gen = input("Gen >> ")
 
-    # print("==============")
-    # Salir
+    print("==============")
 
 
-def consultaEnzimas(gen : str) :
+def consultaEnzimas(gen: str):
     # Listado de clave
-        # dicc.keys()
+    # dicc.keys()
 
     enzima = input("Enzima >> ")
     enzimaTratado = False
 
     # Bucle comprobar nombre enzima
     # Comporbar todas la enzimas con la expresión regular
-        # Si la expresión hace algún tipo de match
-            # tratarEnzima(gen, enzima)
-            # enzimaTratado = True
+    # Si la expresión hace algún tipo de match
+    # tratarEnzima(gen, enzima)
+    # enzimaTratado = True
 
     if not enzimaTratado:
         print("Nombre de enzima incorrecto")
 
 
-def tratarEnzima(gen:str, nombre_enzima:str):
+def tratarEnzima(gen: str, nombre_enzima: str):
     # Pasamos variables del dicc a una forma más manejable
     expresion_enzima = dicc[nombre_enzima][1]
     gorritos_enzima = dicc[nombre_enzima][0]
@@ -116,7 +115,7 @@ def tratarEnzima(gen:str, nombre_enzima:str):
         # Aprendemos que grupo ha hecho match
         i = 1
         while (objeto_match[i] == None):
-          i += 1
+            i += 1
 
         # Añadimos posicion de corte a la lista de cortes
         cortes.append(indice_inicio + objeto_match.start() + gorritos_enzima[i - 1])
@@ -131,20 +130,20 @@ def tratarEnzima(gen:str, nombre_enzima:str):
 
     # Se imprime mensaje si la lista de cortes no está vacia
     if len(cortes) != 0:
-        print(nombre_enzima+" # "+cortes)
+        print(nombre_enzima + " # " + cortes)
 
 
 def abrir_fichero(fichero):
     try:
-        fichero = open(fichero, "r")
-        for linea in fichero:
-            linea = linea.strip()
-            print(linea)
+        with open(fichero, "r") as f:
+            contents = f.read()
+            print(contents)
     except IOError as e:
         print('link_bionet no disponible: ', e)
 
+
 def reemplazar_enzimas(cadena):
-    cadena = re.sub("R", "[AG]",cadena)
+    cadena = re.sub("R", "[AG]", cadena)
     cadena = re.sub("Y", "[CT]", cadena)
     cadena = re.sub("M", "[AC]", cadena)
     cadena = re.sub("K", "[GT]", cadena)
@@ -157,19 +156,16 @@ def reemplazar_enzimas(cadena):
     cadena = re.sub("N", "[ACGT]", cadena)
     return cadena
 
+
 def main():
     cadena_prueba = "UcoMSI (SacI)                     GAGCTCNNNNN^"
-    #Coge y Separa una linea de
-    d = re.search("(\w+) (\(.+\))? +([ATCGRYMKSWBDHVN^]+)",cadena_prueba)
+    # Coge y Separa una linea de
+    d = re.search("(\w+) (\(.+\))? +([ATCGRYMKSWBDHVN^]+)", cadena_prueba)
 
-
-
-
-    print(d.group(1),d.group(2),d.group(3))
+    print(d.group(1), d.group(2), d.group(3))
     reemplazado = reemplazar_enzimas(d.group(3))
     print(reemplazado)
 
 
 if __name__ == '__main__':
     main()
-
